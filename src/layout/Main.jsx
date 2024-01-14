3c6b260387734891adb53a9f6a69b5113be33a5e
Nadezhda Ilina <nadiailina9@gmail.com>
2024-01-14 12:57:55 +0300

    key

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { Movies } from "../components/Movies";
-import { Preloader } from "../components/Preloader";
-import { Search } from "../components/Search";
+import React, { useState, useEffect } from 'react';
+import { Movies } from '../components/Movies';
+import { Preloader } from '../components/Preloader';
+import { Search } from '../components/Search';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
@@ -9,12 +9,12 @@ function Main() {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const searchMovies = (str, type = "all") => {
+  const searchMovies = (str, type = 'all') => {
     setLoading(true);
     fetch(
-      `http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${str}${
-        type !== "all" ? `&type=${type}` : ""
-      }`
+      `http://www.omdbapi.com/?i=tt3896198&apikey=e6135465&s=${str}${
+        type !== 'all' ? `&type=${type}` : ''
+      }`,
     )
       .then((response) => response.json())
       .then((data) => {
@@ -28,7 +28,7 @@ function Main() {
   };
 
   useEffect(() => {
-    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=shrek?`)
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=e6135465&s=shrek?`)
       .then((response) => response.json())
       .then((data) => {
         setMovies(data.Search);
@@ -41,7 +41,7 @@ function Main() {
   }, []);
 
   return (
-    <main className="container content">
+    <main className='container content'>
       <Search searchMovies={searchMovies} />
       {loading ? <Preloader /> : <Movies movies={movies} />}
     </main>
